9a6fc6f816261e24482eabd15639824eaec0891f
lauty94 <lautarogp94@gmail.com>
2025-08-25 20:22:23 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import Boton from "./componentes/boton/Boton"
+import "./App.css"
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+const App = () => {
+
+      function saludo(){
+      console.log("Hola")
+    }
+    saludo()
+
+  return (
+    <>
+    <header>
+      <nav>
+        <h1 className="colorTitulo">Bienvenidos!</h1>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+
+        </ul>
+      </nav>
+    </header>
+    </>
+  )
+}
+
+export default App
